c3a4a813a5fd66af0d07f3cf0e58966e7b758ecf
Kanhaiyasahu01 <kanhaiyasahutools@gmail.com>
2024-08-01 21:36:58 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation";
 import Content from "./components/Content";
 
 function App() {
+  // i will have to create routes , 
   return (
     <div className="App">
      <Navigation/>
